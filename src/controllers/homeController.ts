1863d34cfa25553be009370ae4b3226a62529e9f
GuilhermeUberti <guilherme.uberti@hotmail.com>
2022-05-03 10:04:46 -0300

    Controller e Model criados

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,24 +1,15 @@
 import { Request, Response } from 'express';
 
 import { Product } from '../models/Product';
+import User from '../models/User';
 
-export const home = (req: Request, res: Response)=>{
-    let age: number = 90;
-    let showOld: boolean = false;
+export const home = async (req: Request, res: Response) => {
 
-    if(age > 50) {
-        showOld = true;
-    }
-
-    let list = Product.getAll();
-    let expensiveList = Product.getFromPriceAfter(12);
+    let usuarios = await User.find({});
+    console.log("Usuários: ", usuarios);
 
     res.render('pages/home', {
-        name: 'Bonieky',
-        lastName: 'Lacerda',
-        showOld,
-        products: list,
-        expensives: expensiveList,
-        frasesDoDia: []
+        name: 'Guilherme',
+        lastName: 'Uberti',
     });
 };
